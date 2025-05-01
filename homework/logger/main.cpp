1be5a9c0498ab3153f3cfc4c4f52c9cd2d2b3400
Denys <denddeish1337@gmail.com>
2025-05-01 10:12:32 +0300

    Add support for different case of command

--- homework/logger/main.cpp
+++ homework/logger/main.cpp
@@ -1,10 +1,13 @@
 #include "libraries/logger/logger.h"
+#include <algorithm>
 
 int main(int argc, char* argv[]) {
     SinkType type = SinkType::CONSOLE;
     
     if (argc > 1) {
         std::string arg = argv[1];
+        std::transform(arg.begin(), arg.end(), arg.begin(), ::tolower);
+
         if (arg == "file") type = SinkType::FILE;
         else if (arg == "none") type = SinkType::NONE;
         else if (arg != "console") std::cerr << "Unknown sink type. Using console.\n";
